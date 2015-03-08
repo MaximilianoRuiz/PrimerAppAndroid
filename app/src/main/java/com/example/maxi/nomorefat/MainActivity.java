e72b967c415357c5f8994e1d519710551bdc8411
Maxi <maxi_ruiz140@hotmail.com>
2015-03-08 03:39:23 -0300

    SliderPanelLayout  with Tabs and circle buttons

--- app/src/main/java/com/example/maxi/nomorefat/MainActivity.java
+++ app/src/main/java/com/example/maxi/nomorefat/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.maxi.nomorefat;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SlidingPaneLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -12,50 +14,54 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TabHost;
 
 
 public class MainActivity extends ActionBarActivity {
 
     private ListView listView;
-    private DrawerLayout drawerLayout;
+    private SlidingPaneLayout mSlidingLayout;
     private final String[] opciones = { "Opción 1", "Opción 2", "Opción 3", "Opción 4" };
+    TabHost.TabSpec spec;
+    TabHost tabs;
 
-    private ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = (ListView) findViewById(R.id.list_view);
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        listView = (ListView) findViewById(R.id.left_pane);
+        mSlidingLayout = (SlidingPaneLayout) findViewById(R.id.sliding_pane_layout);
 
-        listView.setAdapter(new ArrayAdapter<String>(this,
+        listView.setAdapter(new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, android.R.id.text1,
                 opciones));
 
-        actionBar = getSupportActionBar();
+        Resources res = getResources();
 
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        tabs=(TabHost)findViewById(android.R.id.tabhost);
+        tabs.setup();
 
-        ActionBar.Tab tab = actionBar.newTab()
-                .setText("Days")
-                .setTabListener(new TabsListener(
-                        this, "Days", DayFragment.class));
-        actionBar.addTab(tab);
+        spec=tabs.newTabSpec("mitab1");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("Days",
+                res.getDrawable(android.R.drawable.ic_btn_speak_now));
+        tabs.addTab(spec);
 
-        tab = actionBar.newTab()
-                .setText("Weeks")
-                .setTabListener(new TabsListener(
-                        this, "Weeks", WeekFragment.class));
-        actionBar.addTab(tab);
+        spec=tabs.newTabSpec("mitab2");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Weeks",
+                res.getDrawable(android.R.drawable.ic_dialog_map));
+        tabs.addTab(spec);
 
-        tab = actionBar.newTab()
-                .setText("Months")
-                .setTabListener(new TabsListener(
-                        this, "Months", MonthFragment.class));
-        actionBar.addTab(tab);
+        spec=tabs.newTabSpec("mitab3");
+        spec.setContent(R.id.tab3);
+        spec.setIndicator("Months",
+                res.getDrawable(android.R.drawable.ic_dialog_map));
+        tabs.addTab(spec);
 
+        tabs.setCurrentTab(0);
     }
 
     @Override
@@ -69,8 +75,8 @@ public class MainActivity extends ActionBarActivity {
     public boolean onKeyUp(int keyCode, KeyEvent event) {
         switch(keyCode){
             case KeyEvent.KEYCODE_BACK:
-                if (drawerLayout.isDrawerOpen(listView)) {
-                    drawerLayout.closeDrawers();
+                if (mSlidingLayout.isOpen()) {
+                    mSlidingLayout.closePane();
                 }else {
                     MainActivity.this.finish();
                 }
@@ -83,10 +89,10 @@ public class MainActivity extends ActionBarActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                if (drawerLayout.isDrawerOpen(listView)) {
-                    drawerLayout.closeDrawers();
-                } else {
-                    drawerLayout.openDrawer(listView);
+                if (mSlidingLayout.isOpen()) {
+                    mSlidingLayout.closePane();
+                }else {
+                    mSlidingLayout.openPane();
                 }
                 return true;
         }
@@ -94,24 +100,4 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public class TabsListener  implements ActionBar.TabListener {
-
-        private Fragment fragment;
-        private final String tag;
-
-        public TabsListener(Activity activity, String tag, Class cls) {
-            this.tag = tag;
-            fragment = Fragment.instantiate(activity, cls.getName());
-        }
-
-        public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-            ft.replace(android.R.id.content, fragment, tag);
-        }
-
-        public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-            ft.remove(fragment);
-        }
-
-        public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {}
-    }
 }
